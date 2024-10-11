ac87cf3601d5640f88a0eed6cd96f01989e683d1
Eduardo Larson <eduardolarson05@gmail.com>
2024-10-10 23:44:39 -0300

    Mensagem do commit

--- src/main/java/org/example/dao/Conexao.java
+++ src/main/java/org/example/dao/Conexao.java
@@ -1,4 +1,15 @@
 package org.example.dao;
 
-public class Conexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao implements IConst {
+    public static Connection getConexao(String stringDeConexao, String usuario, String senha) {
+        try {
+            return DriverManager.getConnection(stringDeConexao, usuario, senha);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
